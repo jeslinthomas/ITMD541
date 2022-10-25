815880f9790bdb1bfcc91b04244678612701459c
druvamurali <druvamurali1@gmail.com>
2022-10-24 23:48:24 -0400

    Add javascript for Lab3.

---
+++ Lab3/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function billFormat(value) {
+  value = Math.ceil(value * 100) / 100;
+  value = value.toFixed(2);
+  return '$' + value;
+}
+
+function splitFormat(value) {
+  if (value == 1 ) return value + ' Person';
+  return value + ' People';
+}
+
+function yourBill() {
+  var bill = Number(document.getElementById ('yourBill').value);
+  var tipPercent = document.getElementById ('tipInput').value;
+  var split = document.getElementById ('splitInput').value;
+  // console.log({bill, tipPercent, split });
+  // console.log('Variables created are working');
+
+  var tipValue = bill * (tipPercent / 100) ;   // console.log(tipValue);
+  var tipEach = tipValue / split;
+  var splitBillEach = ( bill + tipValue ) / split;
+  console.log(splitBillEach);
+
+  document.getElementById('tipPercent').innerHTML = tipPercent + '%';
+  document.getElementById('tipValue').innerHTML = billFormat(tipValue);
+  document.getElementById('totalWithTip').innerHTML = billFormat(bill + tipValue);
+  document.getElementById('splitValue').innerHTML = splitFormat(split);
+  document.getElementById('billEach').innerHTML = billFormat(splitBillEach);
+  document.getElementById('tipEach').innerHTML = billFormat(tipEach);
+}
+
+  var oninput = document.getElementById('oninput');
+  oninput.addEventListener('input', yourBill)
